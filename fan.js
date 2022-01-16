2762c1e30b12c799fa8508c9169b1c5430061080
suymam <82873662+suymam@users.noreply.github.com>
2022-01-16 19:06:26 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,36 @@
+img = "";
+status = "";
+
+function preload(){
+    img = loadImage("fan.jpeg");
+    
+}
+
+function draw(){
+    image(img,0,0,600,420);
+}
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status: detectiong object";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+
+    objectDetector.detect(img , gotResults);
+}
+
+
+function gotResults(error , results){
+    if(error){
+        console.error(error);
+    }
+
+    else{
+        console.log(results);
+    }
+}
